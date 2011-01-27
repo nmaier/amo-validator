3b77a6e4613d89d0e6ef409a6cba53f7afd97ee6
Nils Maier <maierman@web.de>
2011-01-27 04:24:42 +0100

    Fix: Encodings of the json that is passed around
    * Encoding simplified; operating on ascii anyway
    * No need for a temp file (fast io)
    * Minor cleanups

--- validator/testcases/scripting.py
+++ validator/testcases/scripting.py
@@ -1,21 +1,19 @@
-import chardet
 import json
-import os
 import re
 import subprocess
-import tempfile
+# do not use cStringIO as it doesn't support unicode
 from StringIO import StringIO
 
 import validator.testcases.javascript.traverser as traverser
 from validator.constants import SPIDERMONKEY_INSTALLATION
 from validator.contextgenerator import ContextGenerator
-from validator.textfilter import *
 
-JS_ESCAPE = re.compile("\\\\+[ux]", re.I)
+JS_ESCAPE = re.compile(r"\\u")
+WEIRD_CHARS = [chr(c) for c in range(0,32) if "\r\n\t".find(chr(c)) == -1]
 
 def test_js_file(err, filename, data, line=0):
     "Tests a JS file by parsing and analyzing its tokens"
-    
+
     if SPIDERMONKEY_INSTALLATION is None or \
        err.get_resource("SPIDERMONKEY") is None: # Default value is False
         return
@@ -77,7 +75,7 @@ def test_js_file(err, filename, data, line=0):
         if before_tier:
             err.tier = before_tier
         return None
-    
+
     context = ContextGenerator(data)
     if traverser.DEBUG:
         _do_test(err=err, filename=filename, line=line, context=context,
@@ -98,13 +96,13 @@ def test_js_file(err, filename, data, line=0):
 
 def test_js_snippet(err, data, filename, line=0):
     "Process a JS snippet by passing it through to the file tester."
-    
+
     # Wrap snippets in a function to prevent the parser from freaking out
     # when return statements exist without a corresponding function.
     data = "(function(){%s\n})()" % data
 
     test_js_file(err, filename, data, line)
-    
+
 def _do_test(err, filename, line, context, tree):
     t = traverser.Traverser(err, filename, line, context=context)
     t.run(tree)
@@ -112,7 +110,7 @@ def _do_test(err, filename, line, context, tree):
 def _regex_tests(err, data, filename):
 
     c = ContextGenerator(data)
-    
+
     np_warning = "Network preferences may not be modified."
 
     errors = {"globalStorage\\[.*\\].password":
@@ -154,7 +152,6 @@ class JSReflectException(Exception):
         self.line = int(line_num)
         return self
 
-
 def strip_weird_chars(chardata, err=None, name=""):
     line_num = 1
     out_code = StringIO()
@@ -163,21 +160,20 @@ def strip_weird_chars(chardata, err=None, name=""):
     for line in chardata.split(u"\n"):
         charpos = 0
         for char in line:
-            if char >= " " or char in ('\r', '\n', '\t'):
+            if char not in WEIRD_CHARS:
                 out_code.write(char)
-            else:
-                if not has_warned_ctrlchar and err is not None:
-                    err.warning(("testcases_scripting",
-                                 "_get_tree",
-                                 "control_char_filter"),
-                                "Invalid control character in JS file",
-                                "An invalid character (ASCII 0-31, except CR "
-                                "and LF) has been found in a JS file. These "
-                                "are considered unsafe and should be removed.",
-                                filename=name,
-                                line=line_num,
-                                column=charpos,
-                                context=ContextGenerator(chardata))
+            elif not has_warned_ctrlchar and err is not None:
+                err.warning(("testcases_scripting",
+                             "_get_tree",
+                             "control_char_filter"),
+                             "Invalid control character in JS file",
+                             "An invalid character (ASCII 0-31, except CR "
+                             "and LF) has been found in a JS file. These "
+                             "are considered unsafe and should be removed.",
+                             filename=name,
+                             line=line_num,
+                             column=charpos,
+                             context=ContextGenerator(chardata))
                 has_warned_ctrlchar = True
 
             charpos += 1
@@ -188,75 +184,47 @@ def strip_weird_chars(chardata, err=None, name=""):
 
 def _get_tree(name, code, shell=SPIDERMONKEY_INSTALLATION, errorbundle=None):
     "Returns an AST tree of the JS passed in `code`."
-    
+
     if not code:
         return None
-    
-    
-    # Acceptable unicode characters still need to be stripped. Just remove the
-    # slash: a character is necessary to prevent bad identifier errors
-    code = JS_ESCAPE.sub("u", code)
 
-    encoding = None
-    try:
-        code = unicode(code) # Make sure we can get a Unicode representation
-        code = strip_weird_chars(code, errorbundle, name=name)
-    except UnicodeDecodeError:
-        # If it's not an easily decodeable encoding, detect it and decode that
-        code = filter_ascii(code)
+    # Sanitize input
+    code = strip_weird_chars(code, errorbundle, name=name)
+    code = JS_ESCAPE.sub("u", json.dumps(code))
 
-    data = json.dumps(code)
+    # Because of json.dumps code is already ascii
     data = """try{
         print(JSON.stringify(Reflect.parse(%s)));
     } catch(e) {
         print(JSON.stringify({
-            "error":true,
-            "error_message":e.toString(),
-            "line_number":e.lineNumber
+            "error":e.toString(),
+            "line":e.lineNumber
         }));
-    }""" % data
-
-    # Push the data to a temporary file
-    # Cannot use NamedTemporaryFile with delete=True, as on windows such files
-    # cannot be opened by other processes
-    temp = tempfile.NamedTemporaryFile(mode="w+", delete=False)
-    try:
-        temp.write(data)
-        temp.flush() # This is very important
-
-        cmd = [shell, "-f", temp.name]
-        try:
-            shell = subprocess.Popen(cmd,
-                               shell=False,
-                   stderr=subprocess.PIPE,
-                   stdout=subprocess.PIPE)
-        except OSError:
-            raise OSError("Spidermonkey shell could not be run.")
-
-        data, stderr = shell.communicate()
-        if stderr: raise RuntimeError('Error calling %r: %s' % (cmd, stderr))
-
-        # Closing the temp file will delete it.
-    finally:
-        try:
-            temp.close()
-            os.unlink(temp.name)
-        except: pass
+    }""" % code
 
     try:
-        data = unicode(data)
-    except UnicodeDecodeError:
-        data = unicode(filter_ascii(data))
-    
-    parsed = json.loads(data, strict=False)
+        process = subprocess.Popen([shell],
+                         shell=False,
+               stdin=subprocess.PIPE,
+              stderr=subprocess.PIPE,
+              stdout=subprocess.PIPE)
+    except OSError:
+        raise OSError("Spidermonkey shell could not be run.")
 
-    if "error" in parsed and parsed["error"]:
-        if parsed["error_message"][:14] == "ReferenceError":
+    data, stderr = process.communicate(data)
+    if stderr: raise RuntimeError('Error calling %r: %s' % (cmd, stderr))
+
+    try:
+        parsed = json.loads(data, strict=False)
+    except Exception as ex:
+        # shouldn't happen, obviously, but you never know
+        parsed = json.loads('{"error": "failed to parse json", "line": 0}')
+
+    if "error" in parsed:
+        if parsed["error"].startswith("ReferenceError"):
             raise RuntimeError("Spidermonkey version too old; "
                                "1.8pre+ required")
         else:
-            raise JSReflectException(parsed["error_message"]).line_num(
-                    parsed["line_number"])
+            raise JSReflectException(parsed["error"]).line_num(parsed["line"])
 
     return parsed
-
