99b03f5791a9df244944ef909fab7ec621c428c9
Nils Maier <MaierMan@web.de>
2011-06-22 10:47:47 +0200

    Test for implicit declarations from within functions

--- tests/test_js_pollution.py
+++ tests/test_js_pollution.py
@@ -41,3 +41,26 @@ def test_pollution_args():
     (function(c) c = "foo")({});
     (function(d) d = "foo")({});
     """, ignore_pollution=False).failed()
+
+def test_pollution_implicit_from_fun():
+    """
+    Make sure that implicit variable declarations from within functions are caught.
+    Missing a var/let will implicitly declare the variable within the global scope.
+    """
+
+    assert not _do_test_raw("""
+    (function() {
+        a = "foo";
+        b = "foo";
+        c = "foo";
+    })()
+    """, ignore_pollution=False).failed()
+
+    assert _do_test_raw("""
+    (function() {
+        a = "foo";
+        b = "foo";
+        c = "foo";
+        d = "foo";
+    })();
+    """, ignore_pollution=False).failed()
